GabCoolDude/godot

Add support for web mono static libgodot

modules/mono/editor/GodotTools/GodotTools/Utils/Directory.cs
@@ -26,6 +26,40 @@ namespace GodotTools.Utils
             System.IO.Directory.Delete(path.GlobalizePath(), recursive);
         }
 
+        public static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            var globalDestDir = destinationDir.GlobalizePath();
+
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir.GlobalizePath());
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            System.IO.Directory.CreateDirectory(globalDestDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(globalDestDir, file.Name);
+                GodotTools.Utils.File.Copy(file.FullName, targetFilePath);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(globalDestDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
 
         public static string[] GetDirectories(string path, string searchPattern, SearchOption searchOption)
         {

DLSinnocence/godot-net11-web

Co-authored-by: GabCoolDude <gabrielfreville@proton.me>

modules/mono/editor/GodotTools/GodotTools/Utils/Directory.cs
@@ -26,6 +26,33 @@ namespace GodotTools.Utils
             System.IO.Directory.Delete(path.GlobalizePath(), recursive);
         }
 
+        public static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            // Check if the source directory exists
+            if (!Exists(sourceDir))
+                throw new DirectoryNotFoundException($"Source directory not found: {sourceDir.GlobalizePath()}");
+
+            // Cache directories before we start copying
+            string[] dirs = GetDirectories(sourceDir, "*", SearchOption.TopDirectoryOnly);
+
+            // Create the destination directory
+            CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (string file in GetFiles(sourceDir, "*", SearchOption.TopDirectoryOnly))
+            {
+                GodotTools.Utils.File.Copy(file, Path.Combine(destinationDir, file.GetFile()));
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (string subDir in dirs)
+                {
+                    CopyDirectory(subDir, Path.Combine(destinationDir, new DirectoryInfo(subDir).Name), true);
+                }
+            }
+        }
 
         public static string[] GetDirectories(string path, string searchPattern, SearchOption searchOption)
         {